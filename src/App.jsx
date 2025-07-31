304685e1ffc5ebf720452facae56670b53d84e78
joonlee <96dalee@gmail.com>
2025-07-31 14:22:13 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import NumberGuessGame from "./components/NumberGuessGame";
+
+const getRandomTarget = () => Math.floor(Math.random() * 100) + 1;
+
 const App = () => {
-  return <div className="bg-red-100">App</div>;
+  return (
+    <div className="bg-yellow-100 min-h-screen flex justify-center items-center">
+      <NumberGuessGame />
+    </div>
+  );
 };
 
-export default App
+export default App;
